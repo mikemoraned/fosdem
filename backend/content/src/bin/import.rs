76926922b4f7baf402d714cefd10fdcd61916cc9
Mike Moran <mike@houseofmoran.com>
2023-12-25 16:53:22 +0100

    switch to sxd libraries, so I can use XPath

--- backend/content/src/bin/import.rs
+++ backend/content/src/bin/import.rs
@@ -1,4 +1,6 @@
 use clap::{arg, Parser};
+use sxd_document::parser;
+use sxd_xpath::{evaluate_xpath, Value};
 
 /// Load all content from a Pentabarf file
 #[derive(Parser, Debug)]
@@ -13,8 +15,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     let xml = std::fs::read_to_string(args.pentabarf)?;
-    let doc = roxmltree::Document::parse(&xml)?;
-    println!("doc: {:?}", doc);
+    let package = parser::parse(&xml)?;
+    let document = package.as_document();
+    let value = evaluate_xpath(&document, "/schedule")?;
+
+    println!("value: {:?}", value);
 
     Ok(())
 }
