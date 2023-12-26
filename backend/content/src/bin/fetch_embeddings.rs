4256df79ba01bde8556041ca8f92d701000c76d9
Mike Moran <mike@houseofmoran.com>
2023-12-26 20:02:28 +0100

    read from file and look up embeddings

--- backend/content/src/bin/fetch_embeddings.rs
+++ backend/content/src/bin/fetch_embeddings.rs
@@ -3,15 +3,26 @@ use std::fs::File;
 use clap::Parser;
 use dotenv;
 use openai_dive::v1::api::Client;
-use openai_dive::v1::resources::embedding::EmbeddingParameters;
+use openai_dive::v1::resources::embedding::{Embedding, EmbeddingParameters, EmbeddingResponse};
+use serde::Deserialize;
 
 /// Fetch Embeddings
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    /// input csv path
+    #[arg(long)]
+    event_csv: String,
+
     /// output csv path
-    #[arg(short, long)]
+    #[arg(long)]
     embedding_csv: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct EventRecord {
+    title: String,
+    r#abstract: String,
 }
 
 #[tokio::main]
@@ -24,9 +35,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let client = Client::new(api_key);
 
-    let title = "Creative Coding with TurtleStitch";
-    let r#abstract = "<p>Turtlestitch is based on a browser-based educational programming language (Snap!) to generate patterns for embroidery machines. It is easy to use, requiring no prior knowledge in programming, yet powerful in creating nowels patterns for embroidery. It is useful for designers to experiment with generative aesthetics and precision embroidery as well as a tool for innovative workshops combining an introduction to programming with a haptic output.</p>";
-    let input = format!("{} {}", title, r#abstract);
+    let mut event_reader = csv::Reader::from_reader(File::open(args.event_csv)?);
+    let mut embedding_writer = csv::Writer::from_writer(File::create(args.embedding_csv)?);
+    embedding_writer.write_record(&["title", "embedding"])?;
+    for result in event_reader.deserialize() {
+        let event: EventRecord = result?;
+        let response = get_embedding(&client, &event).await?;
+        let embedding = &response.data[0];
+        embedding_writer.write_record(&[event.title, embedding_as_string(embedding)])?;
+    }
+
+    Ok(())
+}
+
+async fn get_embedding(
+    client: &Client,
+    event: &EventRecord,
+) -> Result<EmbeddingResponse, Box<dyn std::error::Error>> {
+    let input = format!("{} {}", event.title, event.r#abstract);
 
     let parameters = EmbeddingParameters {
         model: "text-embedding-ada-002".to_string(),
@@ -35,21 +61,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         user: None,
     };
 
-    let result = client.embeddings().create(parameters).await.unwrap();
-    println!("{:#?}", result);
-    let embedding_string = format!(
-        "[{}]",
-        result.data[0]
-            .embedding
-            .iter()
-            .map(|f| f.to_string())
-            .collect::<Vec<String>>()
-            .join(",")
-    );
+    let response = client.embeddings().create(parameters).await.unwrap();
 
-    let mut csv = csv::Writer::from_writer(File::create(args.embedding_csv)?);
-    csv.write_record(&["title", "embedding"])?;
-    csv.write_record(&[title, &embedding_string])?;
+    Ok(response)
+}
 
-    Ok(())
+fn embedding_as_string(embedding: &Embedding) -> String {
+    embedding
+        .embedding
+        .iter()
+        .map(|f| f.to_string())
+        .collect::<Vec<String>>()
+        .join(",")
 }
