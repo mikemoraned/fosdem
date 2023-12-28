c95248c8bc96743ead2d146eddaa8a7f99256f40
Mike Moran <mike@houseofmoran.com>
2023-12-28 22:11:46 +0100

    loading secrets in fly, just don't use them yet

--- backend/fly/src/bin/fly.rs
+++ backend/fly/src/bin/fly.rs
@@ -1,4 +1,5 @@
 use axum::{http::StatusCode, routing::get, Router};
+use dotenvy;
 
 async fn index() -> &'static str {
     "Hello, world!"
@@ -8,8 +9,24 @@ async fn health() -> StatusCode {
     StatusCode::NO_CONTENT
 }
 
+fn load_secret(name: &str) -> String {
+    let secret = dotenvy::var(&name).expect(&format!("{} is not set", &name));
+    let suffix = secret[(secret.len() - 3)..].to_string();
+    println!(
+        "Loaded secret with name '{}', ending with '{}'",
+        name, suffix
+    );
+    secret
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenvy::dotenv()?;
+
+    let openai_api_key = load_secret("OPENAI_API_KEY");
+    let db_host = load_secret("DB_HOST");
+    let db_key = load_secret("DB_KEY");
+
     let app = Router::new()
         .route("/", get(index))
         .route("/health", get(health));
@@ -18,4 +35,6 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    Ok(())
 }
