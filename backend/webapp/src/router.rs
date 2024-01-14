e3e8c985ab6be51b1ffd5e7540c780ed4c899ece
Mike Moran <mike@houseofmoran.com>
2024-01-14 20:35:39 +0000

    truncate title used in select so that it doesn't look too wide on Mobile

--- backend/webapp/src/router.rs
+++ backend/webapp/src/router.rs
@@ -38,6 +38,36 @@ struct SearchTemplate {
 }
 
 mod filters {
+    use unicode_segmentation::UnicodeSegmentation;
+
+    fn count_graphemes(s: &str) -> usize {
+        UnicodeSegmentation::graphemes(s, true).into_iter().count()
+    }
+
+    pub fn truncate_title(title: &String, max_size: usize) -> ::askama::Result<String> {
+        if count_graphemes(&title) <= max_size {
+            return Ok(title.clone());
+        }
+
+        let suffix = " …";
+        let suffix_length = count_graphemes(suffix);
+        let mut available = max_size - suffix_length;
+
+        let chunks = title.split_word_bounds().collect::<Vec<&str>>();
+        let mut limited = vec![];
+        for chunk in chunks {
+            let chunk_length = count_graphemes(chunk);
+            if available >= chunk_length {
+                limited.push(chunk);
+                available -= chunk_length;
+            } else {
+                break;
+            }
+        }
+        limited.push(suffix);
+        Ok(limited.join(""))
+    }
+
     pub fn distance_similarity(distance: &f64) -> ::askama::Result<String> {
         let similarity = 1.0 - distance;
         Ok(format!("{:.2}", similarity).into())
