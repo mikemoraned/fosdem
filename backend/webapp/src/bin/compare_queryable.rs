918055c220bcfc403bef56a45df4a8f45411fe63
Mike Moran <mike@houseofmoran.com>
2024-01-14 00:17:00 +0000

    go through all and summarise
    - `Error: "differ_on_title: 15"`

--- backend/webapp/src/bin/compare_queryable.rs
+++ backend/webapp/src/bin/compare_queryable.rs
@@ -2,6 +2,7 @@ use std::path::PathBuf;
 
 use clap::Parser;
 use shared::{
+    cli::progress_bar,
     env::load_secret,
     inmemory_openai::InMemoryOpenAIQueryable,
     postgres_openai::PostgresOpenAIQueryable,
@@ -34,6 +35,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     info!("Creating InMemoryOpenAIQueryable");
     let queryable2 = InMemoryOpenAIQueryable::connect(&args.csv_data_dir, &openai_api_key).await?;
 
+    info!("Comparing");
     compare_events(&queryable1, &queryable2).await?;
     compare_related(&queryable1, &queryable2, 5).await?;
 
@@ -48,6 +50,7 @@ where
     T1: Queryable,
     T2: Queryable,
 {
+    info!("Comparing Events");
     let q1_events = queryable1.load_all_events().await?;
     let q2_events = queryable2.load_all_events().await?;
     assert_eq!(q1_events, q2_events);
@@ -65,18 +68,26 @@ where
     T2: Queryable,
 {
     let q1_events = queryable1.load_all_events().await?;
+    info!("Comparing Related");
 
+    let progress = progress_bar(q1_events.len() as u64);
+    let mut differ_on_title = vec![];
     for q1_event in q1_events {
-        let title = &q1_event.title;
-        let q1_related = queryable1.find_related_events(title, limit).await?;
-        let q2_related = queryable2.find_related_events(title, limit).await?;
-        assert_eq!(
-            extract_related_title(title, &q1_related),
-            extract_related_title(title, &q2_related)
-        );
-    }
+        let title = q1_event.title.clone();
+        let q1_related = queryable1.find_related_events(&title, limit).await?;
+        let q2_related = queryable2.find_related_events(&title, limit).await?;
 
-    Ok(())
+        if extract_related_title(&title, &q1_related) != extract_related_title(&title, &q2_related)
+        {
+            differ_on_title.push((title, q1_related, q2_related));
+        }
+        progress.inc(1);
+    }
+    if differ_on_title.len() == 0 {
+        Ok(())
+    } else {
+        Err(format!("differ_on_title: {}", differ_on_title.len()).into())
+    }
 }
 
 fn extract_related_title(title: &String, related: &Vec<SearchItem>) -> Vec<(String, String)> {
