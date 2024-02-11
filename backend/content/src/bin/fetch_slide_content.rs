acd272dac68390db4b305a73c3da8b27d7e45770
Mike Moran <mike@houseofmoran.com>
2024-02-11 16:11:53 +0000

    parse content (temporary-limited to 10 items)

--- backend/content/src/bin/fetch_slide_content.rs
+++ backend/content/src/bin/fetch_slide_content.rs
@@ -35,19 +35,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut event_reader = csv::Reader::from_reader(File::open(&args.event_csv)?);
     info!("Reading events from {} ... ", args.event_csv);
 
-    let mut works = vec![];
+    let mut phase1 = vec![];
     for result in event_reader.deserialize() {
         let work: SlideWork = result?;
-        works.push(work);
+        phase1.push(work);
     }
-    summarise_status(&works);
+    info!("{}", summarise_status(&phase1));
+
+    let phase1: Vec<_> = phase1.into_iter().take(10).collect();
 
     info!("Fetching slide content");
-    let raw_jobs = works
-        .into_iter()
-        .filter(|w| w.slides.len() > 0)
-        .map(|work| async {
-            match fetch_content(&work.slides).await {
+    let mut phase2 = vec![];
+    let phase1_progress = progress_bar(phase1.len() as u64);
+    for work in phase1.into_iter() {
+        if work.slides.len() > 0 {
+            phase2.push(match fetch_content(&work.slides).await {
                 Ok(content) => SlideWork {
                     raw_content: Some(content),
                     ..work
@@ -59,20 +61,48 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     );
                     work
                 }
-            }
-        });
-    let works = join_all(raw_jobs).await;
-    summarise_status(&works);
+            });
+        } else {
+            phase2.push(work);
+        }
+        phase1_progress.inc(1);
+    }
+    info!("{}", summarise_status(&phase2));
+
+    info!("Parsing slide content");
+    let mut phase3 = vec![];
+    let phase2_progress = progress_bar(phase2.len() as u64);
+    for work in phase2.into_iter() {
+        if let Some(raw_content) = &work.raw_content {
+            phase3.push(match parse_content(&raw_content).await {
+                Ok(text) => SlideWork {
+                    text_content: Some(text),
+                    ..work
+                },
+                Err(e) => {
+                    warn!(
+                        "[{}]: got error parsing content for \'{}\': {}",
+                        work.id, work.slides, e
+                    );
+                    work
+                }
+            });
+        } else {
+            phase3.push(work);
+        }
+        phase2_progress.inc(1);
+    }
+    info!("{}", summarise_status(&phase3));
 
     Ok(())
 }
 
-fn summarise_status(works: &Vec<SlideWork>) {
+fn summarise_status(works: &Vec<SlideWork>) -> String {
     let with_slides: Vec<_> = works.iter().filter(|w| w.slides.len() > 0).collect();
     let with_raw_content: Vec<_> = works.iter().filter(|w| w.raw_content.is_some()).collect();
     let with_text_content: Vec<_> = works.iter().filter(|w| w.text_content.is_some()).collect();
-    info!(
-        "total: {}, with: slides: {}, raw: {}, text: {}",
+    format!(
+        "status: total: {}, with: slides: {}, raw: {}, text: {}",
         works.len(),
         with_slides.len(),
         with_raw_content.len(),
@@ -82,6 +112,20 @@ fn summarise_status(works: &Vec<SlideWork>) {
 
 async fn fetch_content(slide_url: &str) -> Result<String, Box<dyn std::error::Error>> {
     let result = reqwest::get(slide_url).await?;
+    if result.status().is_success() {
+        Ok(result.text().await?)
+    } else {
+        Err(format!("non-success: {}", result.status()).into())
+    }
+}
+
+async fn parse_content(raw_content: &String) -> Result<String, Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+    let result = client
+        .put("https://fosdem2024-tika.fly.dev/tika")
+        .body(raw_content.clone())
+        .send()
+        .await?;
     if result.status().is_success() {
         Ok(result.text().await?)
     } else {
