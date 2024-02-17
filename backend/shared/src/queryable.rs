872eb2db2266fdfcd7ca6c8f903454769fc7c367
Mike Moran <mike@houseofmoran.com>
2024-02-17 17:07:30 +0000

    allow(async_fn_in_trait) for `Queryable`

--- backend/shared/src/queryable.rs
+++ backend/shared/src/queryable.rs
@@ -2,6 +2,7 @@ use crate::model::{Event, NextEvents, NextEventsContext, SearchItem};
 
 pub const MAX_RELATED_EVENTS: u8 = 5;
 
+#[allow(async_fn_in_trait)]
 pub trait Queryable {
     async fn load_all_events(&self) -> Result<Vec<Event>, Box<dyn std::error::Error>>;
 
