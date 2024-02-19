37f9f965f243cece8cb6322a4c41d836b73a9d29
Mike Moran <mike@houseofmoran.com>
2024-02-19 20:20:50 +0000

    start on reading of video content

--- backend/content/src/bin/fetch_openai_embeddings.rs
+++ backend/content/src/bin/fetch_openai_embeddings.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::fs::File;
+use std::fs::{DirEntry, File};
 use std::io::{BufReader, BufWriter, Read, Write};
 use std::path::{Path, PathBuf};
 
@@ -7,6 +7,7 @@ use clap::Parser;
 use openai_dive::v1::api::Client;
 
 use openai_dive::v1::resources::embedding::{EmbeddingParameters, EmbeddingResponse};
+use regex::Regex;
 use shared::cli::progress_bar;
 use shared::model::{Event, OpenAIEmbedding};
 
@@ -20,7 +21,11 @@ struct Args {
 
     /// include slide content at path
     #[arg(long)]
-    include_slide_content: Option<String>,
+    include_slide_content: Option<PathBuf>,
+
+    /// include video content at path
+    #[arg(long)]
+    include_video_content: Option<PathBuf>,
 }
 
 #[tokio::main]
@@ -42,9 +47,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("done ");
 
     let mut slide_content_for_event: HashMap<u32, String> = HashMap::new();
-    if let Some(slides_content_path) = args.include_slide_content {
-        println!("Fetching slide content from {} ... ", slides_content_path);
-        let base_path = Path::new(&slides_content_path);
+    if let Some(base_path) = args.include_slide_content {
+        println!("Fetching slide content from {:?} ... ", base_path);
         let mut slide_content_count = 0;
         for event in events.iter() {
             let slide_content_path = base_path.join(event.id.to_string()).with_extension("txt");
@@ -57,6 +61,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
         println!("Read {} events with slide content ", slide_content_count);
+    }
+
+    let mut video_content_for_event: HashMap<u32, String> = HashMap::new();
+    if let Some(base_path) = args.include_video_content {
+        println!("Fetching video content from {:?} ... ", base_path);
+        let mut video_content_count = 0;
+        let dir_entries: Result<Vec<DirEntry>, _> = std::fs::read_dir(base_path)?.collect();
+        let file_regex = Regex::new(r"fosdem-2024-(?<event_id>\d+)-")?;
+        for entry in dir_entries? {
+            let file_name = entry.file_name();
+            if let Some(c) = file_regex.captures(file_name.to_str().unwrap()) {
+                if let Some(m) = c.name("event_id") {
+                    let event_id: u32 = m.as_str().parse().unwrap();
+                    let mut file = File::open(entry.path())?;
+                    let mut content = String::new();
+                    file.read_to_string(&mut content)?;
+                    video_content_for_event.insert(event_id, content);
+                    video_content_count += 1;
+                }
+            }
+        }
+        println!("Read {} events with video content ", video_content_count);
     }
 
     let embedding_path = args.model_dir.join("embeddings").with_extension("json");
