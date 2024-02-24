d9439b0c2e2ee4825721083abb40239bcee22b39
Mike Moran <mike@houseofmoran.com>
2024-02-24 19:12:09 +0000

    add `wav_extraction_stage`

--- backend/content/src/bin/fetch_video_content_chained.rs
+++ backend/content/src/bin/fetch_video_content_chained.rs
@@ -57,6 +57,13 @@ enum AudioExtraction {
     End,
 }
 
+#[derive(Debug)]
+enum WAVExtraction {
+    Command(PathBuf, PathBuf),
+    Aborted,
+    End,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
@@ -84,6 +91,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         mpsc::channel::<VideoDownload>(total_pending_downloads + 1);
     let (audio_extraction_tx, audio_extraction_rx) =
         mpsc::channel::<AudioExtraction>(total_pending_downloads + 1);
+    let (wav_extraction_tx, wav_extraction_rx) =
+        mpsc::channel::<WAVExtraction>(total_pending_downloads + 1);
 
     let multi_progress = MultiProgress::new();
 
@@ -107,8 +116,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         "Extracting audio from videos, saving in {}",
         args.audio_dir.to_str().unwrap()
     );
-    join_set.spawn(extraction_stage(
+    join_set.spawn(audio_extraction_stage(
         audio_extraction_rx,
+        wav_extraction_tx,
+        multi_progress.add(progress_bar(total_pending_downloads as u64)),
+    ));
+
+    info!(
+        "Extracting WAV from audio files, saving in {}",
+        args.audio_dir.to_str().unwrap()
+    );
+    join_set.spawn(wav_extraction_stage(
+        wav_extraction_rx,
         multi_progress.add(progress_bar(total_pending_downloads as u64)),
     ));
 
@@ -130,6 +149,10 @@ fn audio_path(audio_dir: &PathBuf, video_path: &PathBuf) -> PathBuf {
     audio_dir
         .join(format!("{}_audioonly", file_stem.to_str().unwrap()))
         .with_extension("mp4")
+}
+
+fn wav_path(audio_path: &PathBuf) -> PathBuf {
+    audio_path.with_extension("wav")
 }
 
 async fn download_video_stage(
@@ -216,40 +239,57 @@ fn download_video_command(
         .output()
 }
 
-async fn extraction_stage(
+async fn audio_extraction_stage(
     mut audio_extraction_rx: Receiver<AudioExtraction>,
+    wav_extraction_tx: Sender<WAVExtraction>,
     progress: ProgressBar,
 ) -> Result<String, String> {
-    debug!("extraction stage starting");
+    debug!("audio extraction stage starting");
     progress.enable_steady_tick(Duration::from_secs(1));
     while let Some(audio_extraction) = audio_extraction_rx.recv().await {
         use AudioExtraction::*;
 
         match audio_extraction {
             Command(video_path, audio_path) => {
-                if audio_path.exists() {
+                let output = if audio_path.exists() {
                     debug!("{:?} already extracted, skipping", audio_path);
+                    WAVExtraction::Command(audio_path.clone(), wav_path(&audio_path))
                 } else {
                     match extract_audio(&video_path, &audio_path).await {
-                        Ok(_) => (),
+                        Ok(_) => WAVExtraction::Command(audio_path.clone(), wav_path(&audio_path)),
                         Err(e) => {
                             warn!("extract of audio from {:?} failed, {}", video_path, e);
+                            WAVExtraction::Aborted
                         }
                     }
                 };
+                wav_extraction_tx
+                    .send(output)
+                    .await
+                    .map_err(|e| format!("error sending: {}", e))?;
+
                 progress.inc(1);
             }
             Aborted => {
+                wav_extraction_tx
+                    .send(WAVExtraction::Aborted)
+                    .await
+                    .map_err(|e| format!("error sending: {}", e))?;
+
                 progress.inc(1);
             }
             End => {
+                wav_extraction_tx
+                    .send(WAVExtraction::End)
+                    .await
+                    .map_err(|e| format!("error sending: {}", e))?;
                 debug!("finished extraction");
                 break;
             }
         }
     }
 
-    Ok("extraction stage completed".into())
+    Ok("audio extraction stage completed".into())
 }
 
 async fn extract_audio(video_path: &PathBuf, audio_path: &PathBuf) -> Result<(), String> {
@@ -288,6 +328,90 @@ fn extract_audio_command(
         .arg("copy")
         .arg(audio_path.to_str().unwrap())
         .output()
+}
+
+async fn wav_extraction_stage(
+    mut wav_extraction_rx: Receiver<WAVExtraction>,
+    progress: ProgressBar,
+) -> Result<String, String> {
+    debug!("wav extraction stage starting");
+    progress.enable_steady_tick(Duration::from_secs(1));
+    while let Some(wav_extraction) = wav_extraction_rx.recv().await {
+        use WAVExtraction::*;
+
+        match wav_extraction {
+            Command(audio_path, wav_path) => {
+                if wav_path.exists() {
+                    debug!("{:?} wav already extracted, skipping", wav_path);
+                } else {
+                    match extract_wav(&audio_path, &wav_path).await {
+                        Ok(_) => (),
+                        Err(e) => {
+                            warn!("extract of wav from {:?} failed, {}", wav_path, e);
+                        }
+                    }
+                };
+
+                progress.inc(1);
+            }
+            Aborted => {
+                progress.inc(1);
+            }
+            End => {
+                debug!("finished wav extraction");
+                break;
+            }
+        }
+    }
+
+    Ok("wav extraction stage completed".into())
+}
+
+async fn extract_wav(
+    audio_path: &PathBuf,
+    wav_path: &PathBuf,
+) -> Result<(), Box<dyn std::error::Error>> {
+    use std::fs;
+    debug!("extracting {:?} -> {:?}", audio_path, wav_path);
+    let tmp_wav_path = audio_path.with_extension("tmp.wav");
+    debug!("using {:?} as tmp file", tmp_wav_path);
+    if tmp_wav_path.exists() {
+        debug!("removing existing tmp file");
+        fs::remove_file(tmp_wav_path.clone())?;
+    }
+    if wav_path.exists() {
+        debug!("removing existing wav file");
+        fs::remove_file(wav_path.clone())?;
+    }
+    let mut command = async_process::Command::new("/opt/homebrew/bin/ffmpeg");
+    let command = command
+        .arg("-i")
+        .arg(audio_path.to_str().unwrap())
+        .arg("-ar")
+        .arg("16000")
+        .arg("-ac")
+        .arg("1")
+        .arg("-c:a")
+        .arg("pcm_s16le")
+        .arg(tmp_wav_path.to_str().unwrap());
+    debug!("starting extract using command: \'{:?}\'", command);
+    let output = command.output().await?;
+    if output.status.success() {
+        debug!(
+            "extract succeeded, renaming {:?} to {:?}",
+            tmp_wav_path, wav_path
+        );
+        fs::rename(tmp_wav_path, wav_path)?;
+        Ok(())
+    } else {
+        Err(format!(
+            "extract command failed: {}, stdout: {}, stderr: {}",
+            output.status,
+            String::from_utf8(output.stdout)?,
+            String::from_utf8(output.stderr)?
+        )
+        .into())
+    }
 }
 
 fn subset<T>(events: Vec<T>, offset: Option<usize>, limit: Option<usize>) -> Vec<T> {
