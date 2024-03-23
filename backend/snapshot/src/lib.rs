e2e29e4e1a5547aba00abd6449dfacb86fd25a96
Mike Moran <mike@houseofmoran.com>
2024-03-23 16:47:00 +0000

    use Results for errors

--- backend/snapshot/src/lib.rs
+++ backend/snapshot/src/lib.rs
@@ -5,16 +5,19 @@ pub struct Snapshotter {
 }
 
 impl Snapshotter {
-    pub async fn new(openai_api_key: &str, model_dir: &std::path::Path) -> Snapshotter {
+    pub async fn new(
+        openai_api_key: &str,
+        model_dir: &std::path::Path,
+    ) -> Result<Snapshotter, Box<dyn std::error::Error>> {
         let queryable = InMemoryOpenAIQueryable::connect(model_dir, openai_api_key)
             .await
             .unwrap();
-        Snapshotter { queryable }
+        Ok(Snapshotter { queryable })
     }
 
-    pub async fn search(&self, title: &str) -> Vec<SearchItem> {
+    pub async fn search(&self, title: &str) -> Result<Vec<SearchItem>, Box<dyn std::error::Error>> {
         self.queryable.search(title, 20, true).await.unwrap();
-        vec![]
+        Ok(vec![])
     }
 }
 
@@ -30,8 +33,8 @@ mod tests {
     async fn test_phrase() {
         let openai_api_key = load_secret("OPENAI_API_KEY").unwrap();
         let model_dir = PathBuf::from_str("../shared/data/model").unwrap();
-        let snapshotter = Snapshotter::new(&openai_api_key, &model_dir).await;
-        let similar = snapshotter.search("controversial").await;
+        let snapshotter = Snapshotter::new(&openai_api_key, &model_dir).await.unwrap();
+        let similar = snapshotter.search("controversial").await.unwrap();
         insta::assert_yaml_snapshot!(similar);
     }
 }
