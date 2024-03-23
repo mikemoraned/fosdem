1cc8b1a0672f6adfb25421060c58231840a3d8a8
Mike Moran <mike@houseofmoran.com>
2024-03-23 16:45:04 +0000

    call queryable in snapshot but don't yet use result

--- backend/snapshot/src/lib.rs
+++ backend/snapshot/src/lib.rs
@@ -1,25 +1,37 @@
-use shared::model::SearchItem;
+use shared::{inmemory_openai::InMemoryOpenAIQueryable, model::SearchItem, queryable::Queryable};
 
-pub struct Snapshotter;
+pub struct Snapshotter {
+    queryable: InMemoryOpenAIQueryable,
+}
 
 impl Snapshotter {
-    pub fn new() -> Snapshotter {
-        Snapshotter {}
+    pub async fn new(openai_api_key: &str, model_dir: &std::path::Path) -> Snapshotter {
+        let queryable = InMemoryOpenAIQueryable::connect(model_dir, openai_api_key)
+            .await
+            .unwrap();
+        Snapshotter { queryable }
     }
 
-    pub fn find_similar(&self, _phrase: &str) -> Vec<SearchItem> {
+    pub async fn search(&self, title: &str) -> Vec<SearchItem> {
+        self.queryable.search(title, 20, true).await.unwrap();
         vec![]
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::{path::PathBuf, str::FromStr};
+
+    use shared::env::load_secret;
+
     use super::*;
 
-    #[test]
-    fn test_phrase() {
-        let snapshotter = Snapshotter::new();
-        let similar = snapshotter.find_similar("controversial");
+    #[tokio::test]
+    async fn test_phrase() {
+        let openai_api_key = load_secret("OPENAI_API_KEY").unwrap();
+        let model_dir = PathBuf::from_str("../shared/data/model").unwrap();
+        let snapshotter = Snapshotter::new(&openai_api_key, &model_dir).await;
+        let similar = snapshotter.search("controversial").await;
         insta::assert_yaml_snapshot!(similar);
     }
 }
