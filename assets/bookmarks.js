b0c3a1489834d9e3e8d5e6393c887e8fb87decd2
Mike Moran <mike@houseofmoran.com>
2024-12-23 18:51:50 +0100

    loading automerge and deferring or eagerly binding bookmarks

--- assets/bookmarks.js
+++ assets/bookmarks.js
@@ -1,5 +1,15 @@
-export function initBookmarks() {
-    console.log("Initializing Bookmarks");
+// This approach requires the initializeWasm export not yet found in the stable 1.2.1 release.
+import * as AutomergeRepo from "https://esm.sh/@automerge/automerge-repo@2.0.0-alpha.14/slim?bundle-deps"
+import { IndexedDBStorageAdapter } from "https://esm.sh/@automerge/automerge-repo-storage-indexeddb@2.0.0-alpha.14?bundle-deps"
+import { BrowserWebSocketClientAdapter } from "https://esm.sh/@automerge/automerge-repo-network-websocket@2.0.0-alpha.14?bundle-deps"
+import { MessageChannelNetworkAdapter } from "https://esm.sh/@automerge/automerge-repo-network-messagechannel@2.0.0-alpha.14?bundle-deps"
+
+console.log("Initializing Automerge");
+await AutomergeRepo.initializeWasm(fetch("https://esm.sh/@automerge/automerge/dist/automerge.wasm"));
+console.log("Automerge initialized");
+
+export function bindBookmarks() {
+    console.log("Binding Bookmarks");
     // find all bookmark buttons
     const $buttons = Array.prototype.slice.call(
         document.querySelectorAll("button.bookmark"),
@@ -18,5 +28,5 @@ export function initBookmarks() {
     $buttons.forEach((el) => {
         el.disabled = false;
     });
-    console.log("Bookmarks initialized");
+    console.log("Bookmarks bound");
 }
