8b27d60ba7a2c588ffabf119d10f79bc89d9f2b6
Mike Moran <mike@houseofmoran.com>
2024-12-23 18:41:21 +0100

    convert bookmarks to a module

--- assets/bookmarks.js
+++ assets/bookmarks.js
@@ -1,21 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
+export function initBookmarks() {
+    console.log("Initializing Bookmarks");
     // find all bookmark buttons
     const $buttons = Array.prototype.slice.call(
-      document.querySelectorAll("button.bookmark"),
-      0
+        document.querySelectorAll("button.bookmark"),
+        0
     );
-  
+
     // add toggle behavior to each bookmark
     $buttons.forEach((el) => {
-      el.addEventListener("click", () => {  
-        el.classList.toggle("is-filled");
-        el.classList.toggle("is-empty");
-      });
+        el.addEventListener("click", () => {  
+            el.classList.toggle("is-filled");
+            el.classList.toggle("is-empty");
+        });
     });
 
     // enable each bookmark
     $buttons.forEach((el) => {
         el.disabled = false;
     });
-});
-  
+    console.log("Bookmarks initialized");
+}
