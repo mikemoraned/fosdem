7e928b474951c5ed0b8a668b5838589e5f90d275
Mike Moran <mike@houseofmoran.com>
2024-12-25 19:35:40 +0100

    simple setting of value in tinybase

--- assets/model.js
+++ assets/model.js
@@ -1,10 +1,14 @@
-import { createStore } from 'https://cdn.jsdelivr.net/npm/tinybase@5.4.4/+esm';
+import { createMergeableStore } from 'https://cdn.jsdelivr.net/npm/tinybase@5.4.4/+esm';
 
 export class Model {
     constructor() {
+        this.store = createMergeableStore('fosdem2025');
     }
 
     setBookmarkStatus(eventId, status) {
         console.log(`Setting bookmark status for event ${eventId} to ${status}`);
+        this.store.setValue(eventId, status);
+        console.log(this.store.getContent());
+        console.log(this.store.getMergeableContent());
     }
 }
