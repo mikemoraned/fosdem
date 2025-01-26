e87409e4462264edb5e2f3828f231dae077fc41c
Mike Moran <mike@houseofmoran.com>
2025-01-26 21:49:31 +0000

    basic detail page
    - extract simple detail page from search pages
    - make nav links in search go to detail pages

--- webapp/src/router/event.rs
+++ webapp/src/router/event.rs
@@ -1,5 +1,3 @@
-
-
 use askama::Template;
 use axum::{
     extract::{Path, State},
@@ -9,7 +7,7 @@ use axum::{
 use serde::Deserialize;
 use shared::model::{Event, SearchItem};
 use validator::Validate;
-
+use shared::inmemory_openai::InMemoryOpenAIQueryable;
 use crate::filters;
 use crate::state::AppState;
 use shared::queryable::Queryable;
@@ -36,15 +34,32 @@ pub async fn event(
     State(state): State<AppState>,
     Path(EventIdParam(event_id)): Path<EventIdParam>,
 ) -> axum::response::Result<Html<String>> {
-    match state.queryable.find_event_by_id(event_id).await {
-        Ok(Some(event)) => {
-            let related = None;
-            let current_event = None;
-            let page = EventTemplate { event, related, current_event };
-            let html = page.render().unwrap();
-            Ok(Html(html))
-        }
-        _ => Err("failed".into()),
+    // TODO: this is all a bit contorted, for a couple of reasons:
+    // - InMemoryOpenAIQueryable should really natively support finding related events, as opposed to
+    // us having to find by event.title in `find_related_events`
+    // - When we are doing the two calls we can't have a nested await as `dyn StdError` isn't
+    // `Send`, which Rust thinks it needs to be on the second call
+    // Best thing is to move more of the responsibility into `InMemoryOpenAIQueryable` out of here
+    let possible_event : Option<Event> = match state.queryable.find_event_by_id(event_id).await {
+        Ok(event) => event,
+        _ => None
+    };
+    if let Some(event) = possible_event {
+        let current_event = None;
+        let related = find_related_events(&state.queryable, &event).await;
+        let page = EventTemplate { event, related, current_event };
+        let html = page.render().unwrap();
+        Ok(Html(html))
+    }
+    else {
+        Err("failed".into())
+    }
+}
+
+async fn find_related_events(queryable: &InMemoryOpenAIQueryable, event: &Event) -> Option<Vec<SearchItem>> {
+    match queryable.find_related_events(&event.title, 10).await {
+        Ok(related) => Some(related),
+        _ => None,
     }
 }
 
